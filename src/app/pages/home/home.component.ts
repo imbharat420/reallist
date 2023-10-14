1420cf44e07629e2cee95508e9ebd3b1d722fd35
imbharat420 <imbharat420@gmail.com>
2023-10-15 00:42:23 +0530

    button added

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -34,4 +34,32 @@ export class HomeComponent {
       },
     },
   };
+
+  customOptions2: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    nav: true,
+    navText: [
+      '<button type="button" role="presentation" class="owl-prev"><span aria-label="Previous">‹</span></button>',
+      '<button type="button" role="presentation" class="owl-next"><span aria-label="Next">›</span></button>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 2,
+      },
+      740: {
+        items: 3,
+      },
+      940: {
+        items: 4,
+      },
+    },
+  };
 }
